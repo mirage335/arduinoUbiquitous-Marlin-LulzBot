e7d5650d9c4d28168109135e783ad4ba3c8d1c8a
mirage335 <spamfreemirage335@gmail.com>
2020-05-28 19:00:23 -0400

    Remove unnecessary backup.

--- _local/arduino_bak/arduino-1.8.12/examples/Teensy/USB_MIDI/DMX_Lighting/leddmx.h
+++
@@ -1,7 +0,0 @@
-#include <Arduino.h>
-
-void leds_update();
-void led_color(unsigned int num, unsigned int rgb);
-void led_color(unsigned int num, unsigned char red, unsigned char green, unsigned char blue);
-void led_channel(unsigned int num, unsigned int val);
-
